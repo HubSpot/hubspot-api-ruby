09076e46b2ef1510b44e22bfd42bc0d87b173eeb
uyermolkin <uyermolkin@hubspot.com>
2020-01-03 15:04:17 +0300

    [app/contacts-app] Update getting of tokens for actual oauth client

--- sample-apps/contacts-app/app/lib/services/authorization/tokens/generate.rb
+++ sample-apps/contacts-app/app/lib/services/authorization/tokens/generate.rb
@@ -11,12 +11,13 @@ module Services
 
         def call
           tokens_api = ::Hubspot::Client::OAuth::Api::TokensApi.new
-          tokens = tokens_api.postoauthv1token(
+          tokens = tokens_api.post_oauth_v1_token(
             grant_type: :authorization_code,
             code: @code,
             redirect_uri: server_uri + CALLBACK_PATH,
             client_id: ENV['HUBSPOT_CLIENT_ID'],
-            client_secret: ENV['HUBSPOT_CLIENT_SECRET']
+            client_secret: ENV['HUBSPOT_CLIENT_SECRET'],
+            return_type: 'Object'
           )
           tokens[:expires_at] = expires_at(tokens[:expires_in])
           tokens
