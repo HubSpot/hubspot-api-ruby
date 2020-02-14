1e25e68e4e1e47e90fd69c421a3288e427b2c749
Uladzoslau Yermolkin <uyermolkin@hubspot.com>
2020-02-14 13:42:38 +0300

    Revert "[app/redis] Create pure app"

--- sample-apps/redis-sample-app/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get '/oauth', to: 'oauth/authorization#authorize'
-  get '/oauth/callback', to: 'oauth/authorization#callback'
-  get '/login', to: 'oauth/authorization#login'
-  root to: 'home#index'
-end
