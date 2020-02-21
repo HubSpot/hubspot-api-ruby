7568780f7401509dad6569d1a428ce66f33f567c
uyermolkin <uyermolkin@hubspot.com>
2020-02-21 16:46:12 +0300

    [app/leaky-bucket] Add logging

--- sample-apps/leaky-bucket-app/app/workers/leaky_bucket_worker.rb
+++ sample-apps/leaky-bucket-app/app/workers/leaky_bucket_worker.rb
@@ -2,7 +2,7 @@ class LeakyBucketWorker
   include Sidekiq::Worker
 
   def perform(emails, access_token)
-    # sleep 0.2 seconds to avoid 429 error
+    logger.info('Sleep 0.2 seconds to avoid 429 error')
     sleep(0.2)
     response = create_contacts(emails, access_token)
     remove_contacts(response.results.map(&:id), access_token)
@@ -11,10 +11,13 @@ class LeakyBucketWorker
   private
 
   def create_contacts(emails, access_token)
-    Services::Hubspot::Contacts::CreateBatch.new(emails, access_token).call
+    response = Services::Hubspot::Contacts::CreateBatch.new(emails, access_token).call
+    logger.info("Created contacts: #{emails}")
+    response
   end
 
   def remove_contacts(ids, access_token)
     Services::Hubspot::Contacts::ArchiveBatch.new(ids, access_token).call
+    logger.info("Removed contacts: #{ids}")
   end
 end
