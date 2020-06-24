383de60f60ec80db630f554b422385487a201ad7
uyermolkin <uyermolkin@hubspot.com>
2020-06-24 15:41:25 +0300

    [feature/trello-app] Add init flow

--- sample-apps/trello-integration-app/app/controllers/home_controller.rb
+++ sample-apps/trello-integration-app/app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   before_action :authorize
 
   def index
+    @data_fetch_url = url_for(controller: 'trello/cards', action: :index, only_path: false, protocol: 'https')
+    @base_url = url_for(controller: :home, action: :index, only_path: false, protocol: 'https')
   end
 end
