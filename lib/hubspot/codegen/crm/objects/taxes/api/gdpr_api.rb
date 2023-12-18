2056bfa81c178711e872ef7e323999d2c3c4508f
alexey <azheltkovskiy@hubspot.com>
2023-12-18 18:15:26 +0300

    /Users/azheltkovskiy/.vscode/extensions/mateuszdrewniak.ruby-test-runner-0.3.7/print_and_save bundle exec rspec /Users/azheltkovskiy/Desktop/projects/hubspot-api-ruby/hubspot-api-ruby/spec/discovery/crm/objects/goals/gdpr_api_spec.rb:3 /Users/azheltkovskiy/.vscode/extensions/mateuszdrewniak.ruby-test-runner-0.3.7/print_and_save bundle exec rspec /Users/azheltkovskiy/Desktop/projects/hubspot-api-ruby/hubspot-api-ruby/spec/discovery/crm/objects/goals/gdpr_api_spec.rb:3

--- lib/hubspot/codegen/crm/objects/taxes/api/gdpr_api.rb
+++ lib/hubspot/codegen/crm/objects/taxes/api/gdpr_api.rb
@@ -16,14 +16,14 @@ module Hubspot
   module Crm
     module Objects
       module Taxes
-        class GDPRApi
+        class GdprApi
           attr_accessor :api_client
 
           def initialize(api_client = ApiClient.default)
             @api_client = api_client
           end
-          # GDPR DELETE
-          # Permanently delete a contact and all associated content to follow GDPR. Use optional property 'idProperty' set to 'email' to identify contact by email address. If email address is not found, the email address will be added to a blocklist and prevent it from being used in the future.
+          # Gdpr DELETE
+          # Permanently delete a contact and all associated content to follow Gdpr. Use optional property 'idProperty' set to 'email' to identify contact by email address. If email address is not found, the email address will be added to a blocklist and prevent it from being used in the future.
           # @param public_gdpr_delete_input [PublicGdprDeleteInput] 
           # @param [Hash] opts the optional parameters
           # @return [nil]
@@ -32,18 +32,18 @@ module Hubspot
             nil
           end
 
-          # GDPR DELETE
-          # Permanently delete a contact and all associated content to follow GDPR. Use optional property &#39;idProperty&#39; set to &#39;email&#39; to identify contact by email address. If email address is not found, the email address will be added to a blocklist and prevent it from being used in the future.
+          # Gdpr DELETE
+          # Permanently delete a contact and all associated content to follow Gdpr. Use optional property &#39;idProperty&#39; set to &#39;email&#39; to identify contact by email address. If email address is not found, the email address will be added to a blocklist and prevent it from being used in the future.
           # @param public_gdpr_delete_input [PublicGdprDeleteInput] 
           # @param [Hash] opts the optional parameters
           # @return [Array<(nil, Integer, Hash)>] nil, response status code and response headers
           def purge_with_http_info(public_gdpr_delete_input, opts = {})
             if @api_client.config.debugging
-              @api_client.config.logger.debug 'Calling API: GDPRApi.purge ...'
+              @api_client.config.logger.debug 'Calling API: GdprApi.purge ...'
             end
             # verify the required parameter 'public_gdpr_delete_input' is set
             if @api_client.config.client_side_validation && public_gdpr_delete_input.nil?
-              fail ArgumentError, "Missing the required parameter 'public_gdpr_delete_input' when calling GDPRApi.purge"
+              fail ArgumentError, "Missing the required parameter 'public_gdpr_delete_input' when calling GdprApi.purge"
             end
             # resource path
             local_var_path = '/crm/v3/objects/taxes/gdpr-delete'
@@ -74,7 +74,7 @@ module Hubspot
             auth_names = opts[:debug_auth_names] || ['oauth2']
 
             new_options = opts.merge(
-              :operation => :"GDPRApi.purge",
+              :operation => :"GdprApi.purge",
               :header_params => header_params,
               :query_params => query_params,
               :form_params => form_params,
@@ -85,7 +85,7 @@ module Hubspot
 
             data, status_code, headers = @api_client.call_api(:POST, local_var_path, new_options)
             if @api_client.config.debugging
-              @api_client.config.logger.debug "API called: GDPRApi#purge\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+              @api_client.config.logger.debug "API called: GdprApi#purge\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
             end
             return data, status_code, headers
           end
